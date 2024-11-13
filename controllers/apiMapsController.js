642cf79f924ea4d5aa9fa8383f8675e148555cfc
LeandroMassola <leandromassola.lm@gmail.com>
2024-11-13 21:30:09 +0100

    corregi controller

--- controllers/apiMapsController.js
+++ controllers/apiMapsController.js
@@ -2,7 +2,7 @@ module.exports = {
     getIdPlace: async (req, res) => {
         try {
             const { input } = req.query;
-            const apiKey = process.env.GOOGLE_MAPS_API_KEY;  // Define esta clave en tu archivo .env en Render.
+            const apiKey = process.env.GOOGLE_API_KEY;  // Define esta clave en tu archivo .env en Render.
             const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${input}&inputtype=textquery&fields=place_id&key=${apiKey}`;
             const response = await fetch(url);
             const data = await response.json();
