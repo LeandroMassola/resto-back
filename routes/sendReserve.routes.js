829b8f3cfa5d8534d81db95191ecf9afb0b54259
LeandroMassola <leandromassola.lm@gmail.com>
2024-11-13 17:53:22 +0100

    back cambiado

--- routes/sendReserve.routes.js
+++ routes/sendReserve.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const sendReserveController = require('../controllers/sendReserveController')
+const getReviews = require('../controllers/sendReserveController')
 
+
+router.get('/api/maps', getReviews)
 router.post('/sendReserve', sendReserveController.sendSms)
 router.get('/confirmReserve/:reservationId', sendReserveController.getReservationById)
 router.post('/confirmReserve', sendReserveController.confirmReserve)
